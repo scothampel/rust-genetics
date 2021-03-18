909cc367c417c43c2d8e664c0b9ff11a9f488772
Scot Hampel <scot@scothampel.com>
2021-03-17 19:51:27 -0500

    Design change to improve mobile

--- src/PerfectInput.js
+++ src/PerfectInput.js
@@ -22,25 +22,25 @@ export default function PerfectInput(props) {
     }
 
     return (
-        <div className='PerfectInput rounded'>
+        <div className='PerfectInput rounded p-0'>
             <h3 className='text-center w-100'>Starting Near Perfect Gene</h3>
             <form className='row m-0'>
-                <div className='form-group col-md-2'>
+                <div className='form-group col-4 col-md-2 mb-3'>
                     <input type='text' className='form-control gene-input text-center' id='gene1' onChange={handleChange} aria-describedby='gene1' maxLength={1} value={gene.gene1} />
                 </div>
-                <div className='form-group col-md-2'>
+                <div className='form-group col-4 col-md-2 mb-3'>
                     <input type='text' className='form-control gene-input text-center' id='gene2' onChange={handleChange} aria-describedby='gene2' maxLength={1} value={gene.gene2} />
                 </div>
-                <div className='form-group col-md-2'>
+                <div className='form-group col-4 col-md-2 mb-3'>
                     <input type='text' className='form-control gene-input text-center' id='gene3' onChange={handleChange} aria-describedby='gene3' maxLength={1} value={gene.gene3} />
                 </div>
-                <div className='form-group col-md-2'>
+                <div className='form-group col-4 col-md-2 mb-3'>
                     <input type='text' className='form-control gene-input text-center' id='gene4' onChange={handleChange} aria-describedby='gene4' maxLength={1} value={gene.gene4} />
                 </div>
-                <div className='form-group col-md-2'>
+                <div className='form-group col-4 col-md-2 mb-3'>
                     <input type='text' className='form-control gene-input text-center' id='gene5' onChange={handleChange} aria-describedby='gene5' maxLength={1} value={gene.gene5} />
                 </div>
-                <div className='form-group col-md-2'>
+                <div className='form-group col-4 col-md-2 mb-3'>
                     <input type='text' className='form-control gene-input text-center' id='gene6' onChange={handleChange} aria-describedby='gene6' maxLength={1} value={gene.gene6} />
                 </div>
             </form>
